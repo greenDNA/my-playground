d789918bec4d40f9e37499c2705eeeac60f5c489
Trayvont York <trayvont.york@gmail.com>
2020-05-06 16:57:30 -0400

    Updated to have location managed through a constant

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 
+const API_LOCATION = "Brooklyn, New York";
+
 function Weather(){
     const [weatherResource, setWeather] = useState("");
+    const [location, setLocation] = useState(API_LOCATION);
 
     /**
      * Function that when called updates the weatherResource variable 
      * with the current weather of a geographical location
      */
     function refreshWeather(){
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=Brooklyn,usa&appid=cfa213c9a9563140f64787a30ad5a6bc&units=imperial";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${API_LOCATION}&appid=cfa213c9a9563140f64787a30ad5a6bc&units=imperial`;
 
         fetch(url)
         .then(data => data.json())
@@ -20,7 +23,7 @@ function Weather(){
 
     return (
         <div className="wrap-border">
-            <h2>Today's Weather</h2>
+            <h2>Today's Weather in {location}</h2>
             <h4>{weatherResource}</h4>
             <button onClick={refreshWeather}>Click to Update Weather</button>
         </div>
