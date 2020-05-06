59c70d54be6256997fd48c571cbceb5f689c18ee
Trayvont York <trayvont.york@gmail.com>
2020-05-06 17:45:30 -0400

    Updated with dynamic user input
    User is able to enter a location that will be passed to the API and return new data

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 
 const API_LOCATION = "Brooklyn, New York";
 
+/**
+ * TODO: Error handling when user enters invalid data for "location" field
+ * TODO: Further functionality for Weather Component: Sunrise, sunset, rain, snow, etc.
+ */
 function Weather(){
     const [weatherResource, setWeather] = useState("");
     const [location, setLocation] = useState(API_LOCATION);
@@ -11,7 +15,7 @@ function Weather(){
      * with the current weather of a geographical location
      */
     function refreshWeather(){
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${API_LOCATION}&appid=cfa213c9a9563140f64787a30ad5a6bc&units=imperial`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=cfa213c9a9563140f64787a30ad5a6bc&units=imperial`;
 
         fetch(url)
         .then(data => data.json())
@@ -21,11 +25,18 @@ function Weather(){
         .catch( err => console.log(err));
     }
 
+    function updateLocation(e){
+        setLocation(e.target.value);
+    }
+
     return (
         <div className="wrap-border">
             <h2>Today's Weather in {location}</h2>
             <h4>{weatherResource}</h4>
             <button onClick={refreshWeather}>Click to Update Weather</button>
+            <br/>
+            <label htmlFor="">Location</label>
+            <input value={location} onChange={updateLocation} type="text" name="" id=""/>
         </div>
     );
 }
